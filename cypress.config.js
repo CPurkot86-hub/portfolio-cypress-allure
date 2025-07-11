397d5406a5ed71e6a2f835e1affe35fadaf8cde2
Cleverson Purkot <cleverson.purkot@gmail.com>
2025-07-10 23:26:12 -0300

    ajustes finais: config Allure + scripts + Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,15 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureWriter(on, config);
+      allureWriter(on, config); // Habilita o plugin do Allure
       return config;
     },
+    baseUrl: 'http://localhost:3000', // <- Altere se estiver testando outra URL
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}', // Define onde estão os testes
+    supportFile: 'cypress/support/e2e.js'       // Arquivo de suporte (se estiver usando)
   },
+  reporter: 'cypress-mochawesome-reporter', // (opcional se usar outro reporter)
+  env: {
+    allure: true                            // Importante para ativar o plugin
+  }
 });
